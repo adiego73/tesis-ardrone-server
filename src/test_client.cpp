0aebccd8d1447e6cabcab222ec1692f5b78adeba
Diego Avila <adiego73@gmail.com>
2015-07-12 23:51:15 -0300

    add the test client, and configuration to build both main functions

---
+++ src/test_client.cpp
@@ -0,0 +1,87 @@
+#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <errno.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h>
+#include <arpa/inet.h>
+
+#ifndef BUFFER_SIZE
+#define BUFFER_SIZE 256
+#endif
+
+#ifndef PORT_NUMBER
+#define PORT_NUMBER 9090
+#endif
+
+#ifndef HOST_NAME
+#define HOST_NAME "localhost"
+#endif
+
+int main(int argc, char *argv[])
+{
+	int sockfd;
+	struct sockaddr_in serv_addr;
+	struct hostent *server;
+
+	char buffer[BUFFER_SIZE];
+
+	sockfd = socket(AF_INET, SOCK_STREAM, 0);
+
+	if (sockfd < 0)
+	{
+		std::cout << "ERROR on creating socket. Error No: " << errno << std::endl;
+		exit(EXIT_FAILURE);
+	}
+
+	server = gethostbyname(HOST_NAME);
+
+	if (server == NULL)
+	{
+		std::cout << "ERROR there is no host called '"<< HOST_NAME<< "'. Error No: " << errno << std::endl;
+		exit(EXIT_FAILURE);
+	}
+
+	bzero((char *) &serv_addr, sizeof(serv_addr));
+
+	serv_addr.sin_family = AF_INET;
+	inet_aton(server->h_addr, &serv_addr.sin_addr);
+	serv_addr.sin_port = htons(PORT_NUMBER);
+
+	if (connect(sockfd, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) < 0)
+	{
+		std::cout << "ERROR connecting to socket. Error No: "<< errno << std::endl;
+		exit(EXIT_FAILURE);
+	}
+
+	do
+	{
+		std::cout << "MESSAGE: ";
+		std::cin >> buffer;
+
+		if (write(sockfd, buffer, BUFFER_SIZE))
+		{
+			std::cout << "ERROR writing to socket. Error No: " << errno << std::endl;
+			break;
+		}
+
+		bzero(buffer, BUFFER_SIZE);
+
+		if (read(sockfd, buffer, BUFFER_SIZE) < 0)
+		{
+			std::cout << "ERROR reading from socket. Error No: " << errno << std::endl;
+			break;
+		}
+
+		std::cout << "SERVER RESPONSE: " << buffer << std::endl;
+
+	} while (true);
+
+	close(sockfd);
+	return 0;
+}
+
